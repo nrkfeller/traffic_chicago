323989d72e6ddf9f2b395a2eea57413fc4b549c0
Nicolas Feller <me@nicolasfeller.com>
2018-03-30 17:27:53 -0400

    completed dashboard

--- webapp/main.py
+++ webapp/main.py
@@ -1,13 +1,20 @@
-import logging
+from google.cloud import storage
 
-from flask import Flask, render_template, request
+import logging
+import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+SEGMENTS_BLOB = 'segments.json'
+REGIONS_BLOB = 'regions.json'
 
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
+    #regions = readFileFromBucket(REGIONS_BLOB)
     arr = [1, 2, 3]
     return render_template('index.html', value=arr)
 
@@ -21,10 +28,17 @@ def architecture():
 @app.route('/dashboard')
 def dashboard():
     """Return a friendly HTTP greeting."""
-    labels = ["January", "February", "March",
-              "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('dashboard.html', values=values, labels=labels)
+    segments = readFileFromBucket(SEGMENTS_BLOB)
+    regions = readFileFromBucket(REGIONS_BLOB)
+
+    # dt = regions['Downtown Lakefront']
+    # values = reversed(dt[0][-20:])
+    # times = reversed([str(i) for i in dt[1]][-20:])
+
+    return render_template('dashboard.html',
+                           regions=json.dumps(regions),
+                           segments=json.dumps(segments)
+                           )
 
 
 @app.errorhandler(500)
@@ -34,6 +48,14 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+
+def readFileFromBucket(filename):
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket('chicagobucket')
+    blob = bucket.get_blob(filename)
+    return json.loads(blob.download_as_string())
 
 
 if __name__ == '__main__':
